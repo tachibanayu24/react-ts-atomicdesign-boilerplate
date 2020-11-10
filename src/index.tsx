c180628ebbb880dbd723d1f7fbcad81af96f44d9
Yuto Tachibana <tachibanayu24@gmail.com>
2020-11-11 07:20:49 +0900

    :art: Build Atomic Design Structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Button } from "@rtab/components/atoms";
 
 const Index = () => (
-  <h1>Hello!</h1>
+  <>
+    <h1>Hello!</h1>
+    <Button />
+  </>
 )
 
 ReactDOM.render(<Index />, document.getElementById("index"));
