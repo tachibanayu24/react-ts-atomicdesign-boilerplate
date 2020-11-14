f5c45a33a729811318891b715a7f505666b86acd
Yuto Tachibana <tachibanayu24@gmail.com>
2020-11-14 09:53:55 +0900

    :art: Tweak style

--- src/components/molecules/Header/Header.stories.tsx
+++ src/components/molecules/Header/Header.stories.tsx
@@ -2,7 +2,7 @@ import React, { ComponentProps } from "react";
 import { Story } from "@storybook/react/types-6-0";
 import { withKnobs } from "@storybook/addon-knobs";
 import { Header } from ".";
-import { Spacer } from "@rtab/components/atoms";
+import { Spacer, Typography } from "@rtab/components/atoms";
 import { ForkMe } from "@rtab/components/molecules";
 
 export default {
@@ -15,14 +15,14 @@ export const _Default: Story<ComponentProps<typeof Header>> = () => {
     <>
       <Header />
       <Spacer variant="horizontal" size={48} />
-      {[...new Array(12)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-        )
-        .join("\n")}
+      {[...new Array(12)].map((_, i) => (
+        <Typography key={`default__${i}`} variant="body2">
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel
+          scelerisque nisl consectetur et.
+        </Typography>
+      ))}
     </>
   );
 };
@@ -33,14 +33,14 @@ export const WithForkMe: Story<ComponentProps<typeof Header>> = () => {
       <Header />
       <ForkMe />
       <Spacer variant="horizontal" size={48} />
-      {[...new Array(12)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-        )
-        .join("\n")}
+      {[...new Array(12)].map((_, i) => (
+        <Typography key={`default__${i}`} variant="body2">
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel
+          scelerisque nisl consectetur et.
+        </Typography>
+      ))}
     </>
   );
 };
--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { AppBar, Toolbar } from "@material-ui/core";
-import { Typography } from "@rtab/components/atoms";
+import { Typography, Spacer } from "@rtab/components/atoms";
 
 export const Header = () => {
   return (
     <AppBar>
       <Toolbar variant="dense">
-        <Typography variant="body1">React Quiz Sample App</Typography>
+        <Typography variant="subtitle1">React Quiz</Typography>
+        <Spacer variant="vertical" size={24} />
+
+        <Spacer variant="vertical" size={8} />
+        <Typography variant="subtitle2">
+          Atomic Design System Structure
+        </Typography>
       </Toolbar>
     </AppBar>
   );
