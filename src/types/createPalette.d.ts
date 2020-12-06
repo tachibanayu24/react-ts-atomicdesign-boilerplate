73916e4f19716418e9de7a10b1157f5d791577f8
Yuto Tachibana <tachibanayu24@gmail.com>
2020-12-07 07:12:35 +0900

    :art: Refactor

--- src/types/createPalette.d.ts
+++ src/types/createPalette.d.ts
@@ -1,76 +1,5 @@
 import * as createPalette from "@material-ui/core/styles/createPalette";
-
-interface Primary {
-  50: string;
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-  900: string;
-  light: string;
-  main: string;
-  dark: string;
-}
-
-interface Secondary {
-  50: string;
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-  900: string;
-  light: string;
-  main: string;
-  dark: string;
-}
-
-interface Red {
-  50: string;
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-  900: string;
-  A100: string;
-  A200: string;
-  A400: string;
-  A700: string;
-  light: string;
-  main: string;
-  dark: string;
-}
-
-interface Grey {
-  50: string;
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-  900: string;
-  A100: string;
-  A200: string;
-  A400: string;
-  A700: string;
-  light: string;
-  main: string;
-  dark: string;
-}
+import { PaletteColorOptions } from "@material-ui/core/styles/createPalette";
 
 interface White {
   main: string;
@@ -81,10 +10,10 @@ interface Text {
 }
 
 interface CustomPalette {
-  primary: Primary;
-  secondary: Secondary;
-  grey: Grey;
-  red: Red;
+  primary: PaletteColorOptions;
+  secondary: PaletteColorOptions;
+  grey: PaletteColorOptions;
+  red: PaletteColorOptions;
   white: White;
   text: Text;
 }
